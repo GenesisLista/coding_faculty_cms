e8867d1cdda72dbc6fa898f89223642b591178d0
gbl0330 <genesis.bergonia.lista@gmail.com>
2020-05-21 11:46:27 +0300

    Laravel initail structures

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return "Hi about page";
+});
+
+Route::get('/contact', function () {
+    return "Hi contact page";
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "This is post number ".$id." ".$name;
+});
+
+/* rename long url with nickname route */
+Route::get('admin/posts/example', array('as'=>'admin.home', function () {
+
+    $url = route('admin.home');
+    return "This url is " . $url;
+
+}));
+
+Route::group(['middleware' => ['web']], function () {
+    
+});
